6b61b4e7cdb4079932cfadd46396abc3f24ddcca
Annihilater <yanmiexingkong@gmail.com>
2019-03-07 22:28:39 +0800

    feat: Calculate the total length of the video tutorial

--- get_video_files_sum_duration.py
+++ get_video_files_sum_duration.py
@@ -8,39 +8,57 @@ import cv2
 import os
 import re
 
-from config import video_path1 as video_path
+from config import VIDEO_PATH4, VIDEO_PATH
+from running_time import running_time
 
 
-def get_videos_duration(video_folder_path):
-    print(video_folder_path)
-    k = get_files(video_folder_path)
-    data = []
-    for name in k:
-        chapter = re.findall('(\d*)-', name)[0]
-        part = re.findall('-(\d*)', name)[0]
-        data.append([int(chapter), int(part), name])
+@running_time
+def get_videos_duration(path):
+    folder = path.split('/')[-1]
+    print(folder)
+    data = get_files(path)
+    data = parse_file_name(data)
 
-    data = sorted(data)
     sum_time = 0
     for d in data:
         name = d[2]
-        file_path = video_path + '/' + name
+        file_path = path + '/' + name
         time = get_video_duration(file_path)
         sum_time += time  # 单位是秒
         # print(name, time)
 
-    print('秒数', sum_time)
     print('分钟', turn_to_minutes(sum_time))
     print('小时', turn_to_hours(sum_time))
 
 
-def get_files(video_folder_path):
-    g = list(os.walk(video_folder_path))[0][2]
+def get_files(path):
+    g = list(os.walk(path))[0][2]  # 纯的文件名列表
     k = sorted(g)
-    k.remove('.DS_Store')
-    k.remove('课程目录.txt')
+    if 'project.zip' in k:
+        k.remove('project.zip')
+    if 'Python3入门与进阶 源码.zip' in k:
+        k.remove('Python3入门与进阶 源码.zip')
+    if '.DS_Store' in k:
+        k.remove('.DS_Store')
+    if '课程目录.txt' in k:
+        k.remove('课程目录.txt')
     print('获取视频文件 ok')
     return k
+
+
+def parse_file_name(k):
+    data = []
+    for name in k:
+        if name[0].isdigit():  # 处理开头是 1-1 类型的文件名
+            chapter = re.findall('(\d*)-', name)[0]
+            part = re.findall('-(\d*)', name)[0]
+            data.append([int(chapter), int(part), name])
+        elif name[0] == '第':  # 处理开头是 第1章 类型的目录
+            chapter = re.findall('第(.*)章', name)[0]
+            part = 0
+            data.append([chapter, part, name])  # [章, 节, 文件名]
+    data = sorted(data)
+    return data
 
 
 def get_video_duration(path):  # 获取单个视频文件的时长
@@ -68,4 +86,5 @@ def turn_to_hours(seconds):
 
 
 if __name__ == '__main__':
+    video_path = VIDEO_PATH4
     get_videos_duration(video_path)
