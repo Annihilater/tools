701bd4e941c9700923ec80edd18fee2c91513569
Annihilater <yanmiexingkong@gmail.com>
2019-03-07 22:30:31 +0800

    feat: Calculate the running time of the program

--- running_time.py
+++ running_time.py
@@ -4,5 +4,16 @@
 # @Author: PythonVampire
 # @email : vampire@ivamp.cn
 # @File  : running_time.py
+import time
 
 
+def running_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        func(*args, **kwargs)
+        end_time = time.time()
+        used_time = end_time - start_time
+        # 打印结果为 00:05:39 的形式
+        print('运行时间: ', time.strftime('%H:%M:%S', time.gmtime(used_time)))
+
+    return wrapper
